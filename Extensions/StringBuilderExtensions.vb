9c0f8da1128247996bff7161a3b750bf0d81f4b8
MatteoEmmerichRomiri <matteoemmerich@romiri.it>
2025-06-03 17:03:43 +0200

    INIT COMMIT
    Initial commit

---
+++ Extensions/StringBuilderExtensions.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Text
+
+Module StringBuilderExtensions
+    <System.Runtime.CompilerServices.Extension()>
+    Public Sub AppendLineFormat(sb As StringBuilder, format As String, ParamArray args() As Object)
+        sb.AppendLine(String.Format(format, args))
+    End Sub
+End Module
+
